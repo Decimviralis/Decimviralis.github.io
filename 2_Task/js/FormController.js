8f4ac9f6cddcaca94604be50d66bbd6c75df2e23
maxim <decimviralis@gmail.com>
2017-11-03 15:57:25 +0600

    2 commit

--- 2_Task/js/FormController.js
+++ 2_Task/js/FormController.js
@@ -2,11 +2,11 @@ testApp.controller('FormController',
     function FormController($scope, $http){
         $scope.save = function (answer, answerForm){
             if(answerForm.$valid) {
-				alert(answer.author + " " + answer.answer + ", ваш ответ сохранен");
+				alert(answer.author + " " + ", ваш ответ сохранен");
                 console.log(answer.author + ", ваш ответ сохранен");
                 $http.post(
                     'localhost',
-                    JSON.stringify(answer)
+                    JSON.stringify(answer.text)
                 ).success(function (response) {/*success callback*/
                     console.log('success, unreachable')})
             } else {
